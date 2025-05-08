e322abc5d0a01ad5e2d7bfa0a4af17d889f20e97
Kushtrim2024 <kushtro20@yahoo.com>
2025-05-08 10:53:00 +0200

    Export pdf changed

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -42,95 +42,7 @@ export default function ProductDetail({ params }) {
   }
   if (!product) return <div className="p-6">Car not found.</div>;
 
-  // Funktion zum Erstellen der PDF
-
-  const handleExportPDF = async (product) => {
-    try {
-      const imageUrl = product.image[0];
-
-      console.log("Bild URL:", imageUrl);
-      const imgResponse = await fetch(imageUrl);
-      if (!imgResponse.ok) {
-        throw new Error(
-          `Fehler beim Laden des Bildes: ${imgResponse.statusText}`
-        );
-      }
-
-      // Erstellt ein neues PDF-Dokument
-      const pdfDoc = await PDFDocument.create();
-      const page = pdfDoc.addPage([600, 800]);
-      let y = 750;
-
-      // Titel der PDF
-      page.drawText("Fahrzeugdetails- PDF-Dokument ", {
-        x: 50,
-        y,
-        size: 20,
-        color: rgb(0, 0, 0),
-      });
-
-      y -= 40;
-
-      // Fahrzeuginformationen
-      page.drawText(`Name: ${product.name}`, {
-        x: 50,
-        y,
-        size: 14,
-        color: rgb(0.1, 0.1, 0.1),
-      });
-      y -= 24;
-
-      page.drawText(`Preis: €${product.price.toLocaleString()}`, {
-        x: 50,
-        y,
-        size: 14,
-        color: rgb(0.1, 0.1, 0.1),
-      });
-      y -= 24;
-
-      page.drawText(`Beschreibung: ${product.description}`, {
-        x: 50,
-        y,
-        size: 14,
-        color: rgb(0.1, 0.1, 0.1),
-      });
-      y -= 24;
-
-      // const imageUrl = product.image[0];
-      const imgBytes = await imgResponse.arrayBuffer();
-      const img = await pdfDoc.embedJpg(imgBytes);
-      const { width, height } = img.scale(0.4);
-
-      // Bild ins PDF einfügen
-      page.drawImage(img, {
-        x: 50,
-        y: y - height - 20,
-        width,
-        height,
-      });
-
-      // Generiere die PDF Bytes
-      const pdfBytes = await pdfDoc.save();
-
-      // Blob Erstellt öffnet die PDF im neuen Tab
-      const url = URL.createObjectURL(
-        new Blob([pdfBytes], { type: "application/pdf" })
-      );
-
-      // Sicherheitshalber in einem Timeout aufrufen
-      const link = document.createElement("a");
-      link.href = url;
-      link.target = "_blank"; // Öffnet den Link im neuen Tab
-      link.click();
-
-      // URL nach dem Öffnen freigeben
-      setTimeout(() => {
-        URL.revokeObjectURL(url); // URL freigeben
-      }, 1000);
-    } catch (error) {
-      console.error("Fehler beim Erstellen der PDF:", error);
-    }
-  };
+  
   const isFavorite = useMemo(() => {
     return favorites.some((fav) => fav.id === product.id);
   }, [favorites, product.id]);
@@ -192,14 +104,6 @@ export default function ProductDetail({ params }) {
                 className="bg-blue-500 hover:bg-blue-400 cursor-pointer p-2 rounded-lg shadow-md text-xs text-white"
               >
                 {showPhone ? product.sellerPhone : "Show Phone Number"}
-              </button>
-
-              {/* Button zum Erstellen der PDF */}
-              <button
-                onClick={() => handleExportPDF(product)}
-                className="bg-blue-500 p-2 rounded-lg shadow-md text-xs text-white mt-2"
-              >
-                Export as PDF
               </button>
             </div>
           </div>
